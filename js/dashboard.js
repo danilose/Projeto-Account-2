69d3a33188af5b615fb9a986e9ae5c2b04bdce41
LucasVillarim <villarim94@gmail.com>
2021-01-30 08:34:31 -0300

    Adiciona função logout e previne acesso sem auth.

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,30 +1,64 @@
-let baseURL = 'https://accenture-java-desafio.herokuapp.com/';
-let urlParam = 'lancamentos/planos-conta?login=';
+const baseURL = 'https://accenture-java-desafio.herokuapp.com/';
+const planosContaParam = 'lancamentos/planos-conta?login=';
+const dashBoardParam = 'dashboard?fim=2021-01-31&inicio=2021-01-01&login=';
 
-let userData = JSON.parse(localStorage.getItem('usuario'));
-
+const userData = JSON.parse(localStorage.getItem('@usuario'));
+const token = localStorage.getItem('@token');
+console.log(userData);
 let defaultHeader = {
-  'Content-Type': 'application/json',
-  'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsdmlsbGFyaW0xMCIsImlkVXN1YXJpbyI6NDYsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MTE5NTQ3ODQsImV4cCI6MTYxMTk1ODM4NH0.oGcfoW2I0Tlg8DL7LtGvjxMEyk_O7Aek7CrtpVkgMPhMYVVVs2EKqhIFdbLi-5TlnrRl2c954CIu3-S-xG1PEA'
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": localStorage.getItem('@token')
+  }
+}
+
+// Impede o acesso a dash sem autenticação;
+if (!token) {
+  window.location.replace('/');
 }
 
 let tableData = document.getElementById('tableBody');
+let contaNumero = document.getElementById('contaNumero');
+let saldoInput = document.getElementById('saldo');
 
-console.log('chegou');
-axios.get(`${baseURL}${urlParam}lvillarim10`, defaultHeader)
+// Pega os dados referentes aos planos de conta;
+// ENDPOINT na API => /dashboard
+axios.get(`${baseURL}${dashBoardParam}${userData.login}`, defaultHeader)
 .then(response => {
-  let tableBodyContent = response.data.map(element => {
+
+
+  const { contaBanco, contaCredito } = response.data;
+
+
+  console.log(response.data);
+  let tableBodyContent = contaBanco.lancamentos.map(element => {
     return (
       `
       <tr>
         <th scope="row">01/02/2021</th>
         <td>${element.descricao}</td>
-        <td class="red--text">${element.tipoMovimento}</td>
+        <td class="red--text">${element.valor}</td>
       </tr>
       `
     )
   }).join('');
 
+  contaNumero.innerHTML = `Conta nº: ${contaBanco.id}`;
+  saldoInput.innerHTML = contaBanco.saldo.toFixed(2);
   tableData.innerHTML = tableBodyContent;
+});
 
+// Pega os dados da conta do usuário (conta Banco e conta Crédito);
+// ENDPOINT na API => /lancamentos/planos-conta
+axios.get(`${baseURL}${planosContaParam}${userData.login}`, defaultHeader)
+.then(response => {
+  console.log(response.data);
+})
+
+
+// LOGOUT;
+const logoutButton = document.getElementById('logoutButton')
+.addEventListener('click', () => {
+  localStorage.clear();
+  window.location.replace('/');
 });
